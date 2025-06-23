6587c2c0b1dfa804a39b608ddbdf36ba3cece627
NodirJuraqulov <njuraqulov2288@gmail.com>
2025-06-23 15:49:10 +0500

    upload

--- src/pages/shop/hero/HeroShop.jsx
+++ src/pages/shop/hero/HeroShop.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { MdOutlineNavigateNext } from "react-icons/md";
+import heroImg from "@/assets/shophero.png";
 
 const HeroShop = () => {
   return (
-    <div>HeroShop</div>
+    <div style={{ backgroundImage: `url(${heroImg})` }} className='w-full h-[316px] bg-cover bg-center flex flex-col items-center justify-center'>
+      <h2 className='text-[48px] font-medium'>Shop</h2>
+      <div className='flex items-center gap-2'>
+        <p className='text-[16px] font-medium'>Home</p>
+        <MdOutlineNavigateNext />
+        <p className='text-[16px] font-light'>Shop</p>
+      </div>
+    </div>
   )
 }
 
