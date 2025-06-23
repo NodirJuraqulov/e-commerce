902c492b57024195fc664f3081b95633946f63c4
NodirJuraqulov <njuraqulov2288@gmail.com>
2025-06-23 15:34:38 +0500

    upload

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -7,13 +7,19 @@ const Products = ({ data }) => {
   return (
     <div className="grid grid-cols-4 gap-8">
       {data?.map((product) => (
-        <div key={product.id} className="cursor-pointer">
-          <img
-            src={product.thumbnail}
-            alt={product.title}
-            onClick={() => navigate(`/product/${product.id}`)}
-          />
-          <h3>{product.title}</h3>
+        <div key={product.id} className="cursor-pointer bg-[#F4F5F7] p-3">
+          <div className="bg-white">
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              onClick={() => navigate(`/product/${product.id}`)}
+            />
+          </div>
+          <div className="p-2">
+            <h3 className="text-[18px] font-semibold text-[#3A3A3A]">{product.title}</h3>
+            <p className="text-[16px] font-medium text-[#898989]">{product.category}</p>
+            <span className="text-[18px] font-semibold text-[#3A3A3A]">{product.price}$</span>
+          </div>
         </div>
       ))}
     </div>
