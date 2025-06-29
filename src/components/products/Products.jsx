952c951283189671d5fc4b3ba40b96af20f835ad
NodirJuraqulov <njuraqulov2288@gmail.com>
2025-06-29 17:45:51 +0500

    upload

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -27,7 +27,7 @@ const Skeleton = ({ count }) => {
 
 const Products = ({ data, loading, count }) => {
   return (
-    <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid gap-8 grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {loading ? (
         <Skeleton count={count} />
       ) : (
